79f787b9ddb35fab46e00ee63664e6b9f93008b2
BlueBread <hotbread70127@gmail.com>
2017-10-04 18:20:29 +0800

    Add goflib_nisemono.js
    增加goflib.js偽代碼goflib_nisemono.js
    刪除goflib.js中所有註解

--- goflib/goflib/goflib.js
+++ goflib/goflib/goflib.js
@@ -1,8 +1,4 @@
-class Novel extends Object{
-	/* tree: (TreeObj) 作品世界線樹物件
-	author: (Object) 作者資訊
-	intro: (Object) 作品簡介與其他敘述
-	*/
+class Novel{
 	constructor(tree,
 				author={},
 				intro={}){
@@ -12,22 +8,12 @@ class Novel extends Object{
 	}
 };
 class Tree extends Array{
-	// Tree = [[label, twig, twig, ...], [label, twig, twig, ...],...]
-	// twig = [startNode(label), endNode]
-	// label = startNode
-	// 為了輔助畫世界線樹圖，Tree每一個集合的label表示，集合中接下來所有twig皆由label出發。
-	// 在討論畫世界線樹圖的方法時，Route並無影響力，只有表示分歧點的Node才能影響畫圖
-	// 方式。故將所有Route全數刪除，簡化worldLine，只留下Node。 twig表示一組由Node
-	// 組成之線段。
 	constructor(...worldLineBundle){
 		const twigBox = Tree.simplifyWLs(worldLineBundle);
 		const seedling = Tree.sortNode(twigBox);
 		super(...seedling);
 	}
 	static simplifyWLs(worldLineBundle){
-		// 將所有世界線，先將所有Route摘除並留下Node，
-		// wl前後一組形成twig，貯存進twigBox([1,2,3,4]=>[[1,2],[2,3],[3,4]])
-		// 回傳twigBox
 		let twigBox = [];
 		for (let wl of worldLineBundle){
 			Tree.cutRoute(wl, 0);
@@ -37,11 +23,6 @@ class Tree extends Array{
 		return twigBox;
 	}
 	static cutRoute(wl, startPosi){
-		// 從startPoint出發，若檢查接下來的物件是否為Node.prototype
-		// 若非，count + 1；若是Node，迴圈檢查結束
-		// 接著，從startPoint開始，刪除count個
-		// 最後，如果使迴圈檢查結束的Node(因為世界線被清理過後，位置為startPoint + 1)
-		// 為"Terminator"，則清理結束；若非，從startPoint + 1繼續遞迴
 		let count = 0;
 		while(wl[startPosi+count+1].__proto__ != Node.prototype){
 			count += 1;
@@ -58,7 +39,6 @@ class Tree extends Array{
 		}
 	}
 	static makeTwigs(wl, twigBox){
-		// wl中，前後兩個Diverg拆成一個twig，並將所有twig貯存進twigBox
 		for(var i=0; i < wl.length-1 ; i++){
 			let twig = [];
 			twig.push(wl[i], wl[i+1]);
@@ -66,11 +46,6 @@ class Tree extends Array{
 		}
 	}
 	static sortNode(twigBox){
-		// Tree結構為[[label, twig, twig, ...], ...]，其中label宣告此陣列中所有twig
-		// 皆由label出發。
-		// 在twigBox中檢查所有twig，若twig出發點(label)，已在Tree中被建立，創立一個以
-		// 所有由新label出發的twig集合([label, twig, twig, ...])；若非，尋找此label在Tree
-		// 中帶領的集合，最後將twig加入此集合。
 		let seedling = [];
 		let seedling_labels = [];
 		for (const twig of twigBox){
@@ -84,8 +59,6 @@ class Tree extends Array{
 		return seedling
 	}
 	static createNewLabel(seedling, label){
-		// 在Tree中新創立一個由label領導的集合。
-		// 為了防笨，再度檢查此label在Tree中並無領導的集合。若確實集合不存在，創立新集合。
 		let doubleCheck = true;
 		for (const branch of seedling){
 			(branch[0] == label) && (doubleCheck = false);
@@ -98,8 +71,6 @@ class Tree extends Array{
 		}
 	};
 	static addNewTwig(seedling, twig){
-		// 將twig加入Tree中
-		// 以label來搜尋此twig所屬的集合，最後加入此label所帶領的集合。
 		const label = twig[0];
 		for (const branch of seedling){
 			branch[0] == label && branch.push(twig);
@@ -114,9 +85,6 @@ class WorldLine extends Array{
 		this.posiProgress = 0;
 		this.progress = this[0];
 	}
-	/* WorldLine.prototype.progress傳回現在WorldLine的檢查點，next()傳回檢查
-	點的下一個物件，last()傳回檢查點上一個物件。
-	*/ 
 	next(){
 		this.posiProgress++;
 		this.progress = this[this.posiProgress];
@@ -141,11 +109,6 @@ class WorldLine extends Array{
 		}
 	}
 	graft(newRoute, posi){
-		/* WorldLine中，依照傳入posi所表達的位置，如果此位置上為Node，此WorldLine
-		不變，將新Route嫁接上，最後建立並創立新WorldLine; 如果此位置上為Elastic，
-		函數傳回Error; 如果此位置上為Route，Route後新建一個Node，再將新Route嫁接上，
-		最後建立並創立新WorldLine。
-		*/		
 		if (newRoute.__proto__ != Route.prototype) {
 			throw 'This is not a Route Object...';
 			return undefined;
@@ -186,30 +149,16 @@ class Route extends Array{
 	constructor(...chaps){
 		super(...chaps);
 	}
-  	// pre(){};
-  	// next(){};
-  	// grow(){};
-  	// graft(){};
 };
 
 class Chap extends String{
-	/* 章節。構成Tree物件中的基礎單位*/
 	constructor(content){
 		super(content);
 	}
-	//graft(newChap){
-	//	/* WorldLine中，在此Chap之後插入新的Node分歧點。
-	//	然後將新Chap分支嫁接上去，最後創建並回傳一個新WorldLine。
-	//	*/
-	//}
 };
 
 
 class Node extends Set{
-	/* 分歧。於WorldLine中表示節點，方便判別WorldLines相對關係和Tree結構。
-	Node中貯存分歧的Routes，以及分歧前('Split')/收束後('Elastic')"唯一"接續
-	的root。
-	*/
 	constructor(root,
 				attr='Split',
 				routes=undefined){
@@ -222,11 +171,6 @@ class Node extends Set{
 			throw new NodeAttrException(attr).message;
 		}
 	}
-	// graft(newChap){
-	// 	 /*WorldLine中，然後將新Chap分支嫁接上去此Diverg上。
-	// 	若Diverg.attr為'Split'，最後創建並回傳一個新WorldLine。
-	//	*/
-	// }
 };
 class NodeAttrException{
 	constructor(attr){
